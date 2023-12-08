1f7f460aa51e7b11a2df1336b36f7a2f7bada881
Woong Jee <uccawkj@login02.ib.young.ucl.ac.uk>
2023-12-08 12:07:23 +0000

    intel compiler testing 1

--- Example/testrun_template_python/SGE_js.sh
+++ Example/testrun_template_python/SGE_js.sh
@@ -12,17 +12,27 @@
 #$ -N tf_sge
 
 # Select the MPI parallel environment and 16 processes.
-#$ -pe mpi 160
+#$ -pe mpi 80
 
 # Set the working directory to somewhere in your scratch space.
 # Replace "<your_UCL_id>" with your UCL user ID :
 #$ -cwd
 
-#$ -P Gold
+
+## using -P Test > limited to 2 nodes for 1 hour wtime
+#$ -P Test
 #$ -A UCL_chemM_Woodley
 
 # Run our MPI job.  GERun is a wrapper that launches MPI jobs on our clusters.
 # ulimit -s unlimited
 export OMP_NUM_THREADS=1
 
-gerun /path/to/taskfarm.x 1> stdout 2> stderr
+# USE PYTHON - CHANGE THIS FOR YOUR ACCOUNT SPECIFICATION
+source /home/uccawkj/miniconda3/bin/activate
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/uccawkj/miniconda3/lib/
+# USE PYTHON
+
+# IF USING UCL MACHINE WITH 'gerun'
+# gerun /path/to/taskfarm.x 1> stdout 2> stderr
+# ELSE (that supporting 'mpirun' cmd)
+mpirun -np 80 /path/to/tf.x
